c796b3c69e67d9a4c9e875fc4ce495291f316328
Guslington <guslington@gmail.com>
2018-10-17 17:58:26 +1100

    sns payload triggers

--- shelvery_lambda/lambda_handler.py
+++ shelvery_lambda/lambda_handler.py
@@ -4,29 +4,35 @@ import json
 from shelvery.factory import ShelveryFactory
 
 def lambda_handler(event, context):
-    
+
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     logger.info(f"Received event\n{json.dumps(event,indent=2)}")
-    
-    if 'backup_type' not in event:
+
+    # handle both sns and cloudwatch secheduled events
+    if 'Records' in event:
+        payload = event['Records'][0]['Sns']['Message']
+    else:
+        payload = event
+
+    if 'backup_type' not in payload:
         raise Exception("Expecting backup type in event payload in \"backup_type\" key")
 
-    if 'action' not in event:
+    if 'action' not in payload:
         raise Exception("Expecting backup action in event payload in \"action\" key")
 
-    backup_type = event['backup_type']
-    action = event['action']
+    backup_type = payload['backup_type']
+    action = payload['action']
 
     # create backup engine
     backup_engine = ShelveryFactory.get_shelvery_instance(backup_type)
-    backup_engine.set_lambda_environment(event, context)
-    
+    backup_engine.set_lambda_environment(payload, context)
+
     method = backup_engine.__getattribute__(action)
-    
-    if 'arguments' in event:
+
+    if 'arguments' in payload:
         method(event['arguments'])
     else:
         method()
-    
+
     return 0
