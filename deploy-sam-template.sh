9db274a9c8e41060dd620d12615e70b842fc66a3
Guslington <guslington@gmail.com>
2020-05-21 17:28:02 +1000

    update sam template package and deployment to use the sam cli with ability to set parameter overrides

--- deploy-sam-template.sh
+++ deploy-sam-template.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -e
+set -ex
 
-SHELVERY_VERSION=0.9.4
+SHELVERY_VERSION=0.9.5
 
 # set DOCKERUSERID to current user. could be changed with -u uid
 DOCKERUSERID="-u $(id -u)"
 
-while getopts ":b:fa:r:u:l:p:v" opt; do
+while getopts ":b:r:u:l:p:v:o:" opt; do
   case $opt in
     b)
       BUCKET=$OPTARG
@@ -23,8 +23,8 @@ while getopts ":b:fa:r:u:l:p:v" opt; do
     p)
       PACKAGE=$OPTARG
       ;;
-    f)
-      CLOUDFORMOPTIONS="--force-upload"
+    o)
+      PARAM_OVERRIDES=$OPTARG
       ;;
     u)
       DOCKERUSERID=" -u $OPTARG"
@@ -69,20 +69,22 @@ cd lib
 zip shelvery.zip -r ./*
 cd ..
 
+PARAM_OPTS=""
+
+if [ ! -z ${PARAM_OVERRIDES} ]; then
+  PARAMS=($(echo ${PARAM_OVERRIDES} | tr ',' "\n"))
+  PARAM_OPTS="--parameter-overrides"
+  for p in "${PARAMS[@]}"
+  do
+    KEY=$(echo $p | cut -d'=' -f1)
+    VALUE=$(echo $p | cut -d'=' -f2)
+    PARAM_OPTS="${PARAM_OPTS} ParameterKey=${KEY},ParameterValue=${VALUE}" 
+  done
+fi
+
 echo "packaging cloudformation"
-aws cloudformation package \
-  ${CLOUDFORMOPTIONS} \
-  --template-file template.yaml \
-  --s3-bucket $BUCKET \
-  --s3-prefix cloudformation/shelvery \
-  --output-template-file packaged-template.yaml \
-  $REGION
+sam package --template-file template.yaml --s3-bucket $BUCKET --s3-prefix cloudformation/shelvery --output-template-file packaged-template.yaml $REGION
+
 
 echo "updating/creating cloudformation stack shelvery"
-aws cloudformation deploy \
-  ${CLOUDFORMOPTIONS} \
-  --no-fail-on-empty-changeset \
-  --template-file ./packaged-template.yaml \
-  --stack-name shelvery \
-  --capabilities CAPABILITY_IAM \
-  $REGION
+sam deploy --template-file ./packaged-template.yaml --stack-name shelvery --capabilities CAPABILITY_IAM $PARAM_OPTS $REGION
